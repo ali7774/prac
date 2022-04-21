fc162254177b02a463fb96e99286e9d449717d3f
ali7774 <hafizalihaider134@gmail.com>
2022-04-22 00:06:36 +0500

    Ali

---
+++ Untitled4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+float circumference(float &r){
+	return(2*3.14*r);
+}
+float area(float &r){
+	return(3.14*r*r);
+}
+int main(){
+	float a,c,r;
+	cout<<"enter radius:"<<endl;
+	cin>>r;
+	c=circumference(r);
+	a=area(r);
+	cout<<"circumference is:"<<c<<endl;
+	cout<<"area is:"<<a<<endl;
+}
